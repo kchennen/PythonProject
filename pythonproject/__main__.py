ce23ec45af3a345de7bcc7a858531fec7da4d810
Kirsley Chennen <kirsley.chennen@gmail.com>
2020-10-01 00:00:31 +0200

    Update project source code

---
+++ pythonproject/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# This is a sample Python script.
+
+def main(name='World'):
+    print('Hello, {name}!'.format(name=name))  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    main()
