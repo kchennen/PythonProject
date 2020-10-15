2ae7a52cbaae666de97a18ed058b0374220bcebe
Kirsley Chennen <kirsley.chennen@gmail.com>
2020-10-15 02:51:09 +0200

    Update requirements.txt and tests

--- setup.py
+++ setup.py
@@ -13,13 +13,42 @@ def requirements():
 setup(
     name='PythonProject',                                       # name of the package
     version='0.1.0',                                            # version of this release
+    url='https://github.com/kchennen/PythonProject',            # home page for the package
+    download_url='https://github.com/kchennen/PythonProject',   # location where the release version may be downloaded
     author='kchennen',                                          # package author’s name
     author_email='author@mail.com',                             # email address of the package author
-    url='https://github.com/kchennen/PythonProject',            # home page for the package
     description='Python project template',                      # short, summary description of the package
+    long_description=read('README.md'),                         # longer description of the package (used by PyPI when you publish a package, to build its project page)
     license='MIT',                                              # license for the package
-    download_url='https://github.com/kchennen/PythonProject',   # location where the release version of this package may be downloaded
+    keywords=(                                                  # list of keywords describing the package
+        "Python, cookiecutter, unittest, project "
+        "templates, example, documentation, tutorial, setup.py, package"
+    ),
     install_requires=requirements(),                            # install external packages as dependencies
-    packages=['pythonproject'],
-    scripts=['bin/pythonproject']
+    packages=['pythonproject'],                                 # same as name
+    scripts=['bin/pythonproject'],                              # Scripts are files intended to be started from the command line
+    entry_points={                                              # Register the main() function of the package
+        'console_scripts': ['pythonproject=pythonproject.__main__:main'],
+    },
+    classifiers=[                                               # list of classifiers to categorize each release
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: X11 Applications',
+        'Framework :: Flake8',
+        'Framework :: Flask',
+        'Framework :: Jupyter',
+        'Framework :: Matplotlib',
+        'Framework :: Pytest',
+        'Framework :: Setuptools Plugin',
+        'Framework :: tox',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],                                          # list of classifiers to categorize each release
 )
