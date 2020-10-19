f559db09e87b48e54d07e1a1a08c006e25b70b6b
Kirsley Chennen <kchennen@unistra.fr>
2020-10-19 14:42:45 +0200

    Test Bug Fix requirements.txt

--- pythonproject/__main__.py
+++ pythonproject/__main__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def main(name):
-    if not len(name):
-        name = 'World'
-
+def main(name='World'):
     result = 'Hello, {name}!'.format(name=name)
     print(result)
     return result
--- tests/test_pythonproject.py
+++ tests/test_pythonproject.py
@@ -5,6 +5,10 @@ from pythonproject.__main__ import main
 class PythonProjectTest(unittest.TestCase):
     """Test  case for PythonProject package"""
 
-    def test_main(self, name='Test'):
+    def test_main1(self):
+        """Test Main function of PythonProject"""
+        self.assertEqual(main(), 'Hello, World!')
+
+    def test_main2(self, name='Test'):
         """Test Main function of PythonProject"""
         self.assertEqual(main(name=name), 'Hello, Test!')
